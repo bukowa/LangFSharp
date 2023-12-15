da2ea50039d60490099a1d185f244f7db5ed0a0a
bukowa <gitbukowa@gmail.com>
2023-12-15 16:54:56 +0100

    feat: add script `aasync.fsx`
    Signed-off-by: bukowa <gitbukowa@gmail.com>

---
+++ Basic/README.md
@@ -0,0 +1,6 @@
+
+`.fsx` is a script file, which can be run with `dotnet fsi` command.
+
+```bash
+dotnet fsi aasync.fsx
+```
---
+++ Basic/aasync.fsx
@@ -0,0 +1,36 @@
+﻿#load "aasync.fs"
+open System
+open basic.aasync
+
+aasync.add 1 2 |> Async.StartImmediateAsTask |> printf "%A\n"
+// System.Threading.Tasks.Task`1[System.Int32]
+
+aasync.add 1 2 |> Async.StartChild |> printf "%A\n"
+// Microsoft.FSharp.Control.FSharpAsync`1[Microsoft.FSharp.Control.FSharpAsync`1[System.Int32]]
+
+let addprintfn x y =
+    async {
+        let! o = aasync.add x y
+        printfn $"%d{o}"
+        return o
+    }
+
+let multiaddprintf =
+    [|1;2;3;4;5;6;7;8;9;0|]
+    |> Seq.pairwise
+    |> Seq.map (fun(x,y) -> addprintfn x y)
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    
+multiaddprintf
+
+let keepAdding x =
+    async {
+        while true do
+            Random().Next(1,100)  |>
+            aasync.add x |> Async.StartAsTask |> ignore
+            printf $"{x}\n"
+            do! Async.Sleep 1
+        }
+
+Seq.init 10000 keepAdding |> Async.Parallel |> Async.RunSynchronously |> ignore
